b97c7db0f6c65d98107999d9cd40b0eb64bdf7b2
Neerajajayakumar555 <neerajajayakumar9@gmail.com>
2026-06-25 00:03:24 +0530

    Fixed app.py for Render

--- app.py
+++ app.py
@@ -22,9 +22,7 @@ def home():
 
     return render_template("index.html", sentiment=sentiment)
 
-if __name__ == "__main__":
-    app.run(debug=True)
-    import os
+import os
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
