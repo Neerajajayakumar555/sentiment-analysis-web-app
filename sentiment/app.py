0ee8a97af249ffba2c54d915668c7dce4557f7ad
Neerajajayakumar555 <neerajajayakumar9@gmail.com>
2026-06-25 20:24:23 +0530

    Add files via upload

---
+++ sentiment/app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request
+from textblob import TextBlob
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    sentiment = ""
+
+    if request.method == "POST":
+        text = request.form["text"]
+
+        analysis = TextBlob(text)
+        polarity = analysis.sentiment.polarity
+
+        if polarity > 0:
+            sentiment = "Positive 😊"
+        elif polarity < 0:
+            sentiment = "Negative 😞"
+        else:
+            sentiment = "Neutral 😐"
+
+    return render_template("index.html", sentiment=sentiment)
+
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
